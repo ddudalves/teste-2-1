4617474807a03d91b96b7fe1ac0a86c3803015c0
ddudalves <169068338+ddudalves@users.noreply.github.com>
2024-09-02 12:09:56 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa que soma dois números");
+  
+System.out.println("O primeiro número é:");
+    
+    int numero1 = 4;
+    
+    System.out.println(numero1);
+
+    System.out.println("O segundo número é:");
+
+  int numero2 = 9;
+  
+  System.out.println(numero2);
+
+    System.out.println("A soma é:");
+      int soma = numero1 + numero2;
+
+    System.out.println(soma);
+  
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
